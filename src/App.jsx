992efaf3c87c852b0c83fe919523ff05c0752f89
Jonathan Echeverria Armesto <jonathan.eche@gmail.com>
2025-11-24 22:15:01 +0100

    Performaces section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
-import gsap from "gsap";
-import { ScrollTrigger} from "gsap/all"
-import Showcase from "./components/Showcase.jsx";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -15,7 +18,12 @@ const App = () => {
             <Hero />
             <ProductViewer />
             <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
         </main>
     )
 }
+
 export default App
